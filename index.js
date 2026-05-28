cce9eb140ffe3f41d23b4d11614c61a60a8aecb0
Sam7ka <samuelhenrique190510@gmail.com>
2026-05-28 11:36:45 -0300

    Add files via upload
    vai até a questão 3

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Bem-vindo ao sistema!');
+  res.send('Testando o express!');
 });
+
+app.get('/sobre', (req, res) => {
+  res.send('segunda questão questão');
+});
+//3
+app.get('/contato', (req, res) => {
+    res.json({
+    "email": "contato@email.com",
+    "telefone": "(81) 99999-9999"
+    });
+  res.send('segunda questão questão');
+});
+
 app.listen(
     3000, 
     () => console.log(`Bem-vindo ao sistema`)
